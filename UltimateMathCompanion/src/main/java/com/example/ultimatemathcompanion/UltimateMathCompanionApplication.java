1db8424968326c91a88cf80cb4f7b6f32e230cf6
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-10-10 22:49:13 +0200

    Initial project

---
+++ UltimateMathCompanion/src/main/java/com/example/ultimatemathcompanion/UltimateMathCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.example.ultimatemathcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateMathCompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateMathCompanionApplication.class, args);
+    }
+
+}
---
+++ UltimateMathCompanion/src/test/java/com/example/ultimatemathcompanion/UltimateMathCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ultimatemathcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateMathCompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
