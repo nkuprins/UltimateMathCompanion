e02254f43d80373abcc45cdd0bda5e32b8bd48f0
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-10-25 17:37:51 +0200

    Add highlight first row on page load.

--- UltimateMathCompanion/src/main/resources/static/mainScript.js
+++ UltimateMathCompanion/src/main/resources/static/mainScript.js
@@ -3,3 +3,10 @@ function highlightRow(row) {
     rows.forEach(el => el.className='')
     row.className  = 'bg-info';
 }
+function highlightFirstRow() {
+    const rows = document.querySelectorAll("tr");
+    rows[1].className = 'bg-info';
+}
+window.onload = function() {
+    highlightFirstRow();
+};
