82990efd37a2b9c107c8efc4cf6f264f38d51b20
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-11-21 23:02:56 +0100

    Add seperate .js files for function. Form is for alert of invalid format. Main is for selecting row in the list.

---
+++ UltimateMathCompanion/src/main/resources/static/form.js
@@ -0,0 +1,9 @@
+
+function createErrorAlert() {
+    alert("Invalid format of data\n" +
+    "Possible mistakes:\n" +
+        "1. The text is empty\n" +
+        "2. The text contains invalid symbols like: 'A','$','!'\n" +
+        "3. The text contains has DECIMAL numbers but it should be INTEGERS.\n" +
+        "4. The text does not have enough spaces or they are redundant");
+}
