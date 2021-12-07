150898ea3d003e1ccfb8200fa3f10aca2528dabe
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-12-07 01:01:51 +0100

    Remove update and delete buttons for each row and add one general group of buttons. Resize table and centre the text in it.

--- UltimateMathCompanion/src/main/resources/static/main.js
+++ UltimateMathCompanion/src/main/resources/static/main.js
@@ -1,13 +1,24 @@
 
-function selectRow(row) {
+let selectedId;
+
+function selectRow(row, id) {
     const rows = document.getElementById("expressions-list").querySelectorAll("tr");
-    rows.forEach(el => el.className='')
-    row.className  = 'bg-info';
+    rows.forEach(el => el.className='text-center')
+    row.className  = 'bg-info text-center';
+    selectedId = id;
 }
 
 function selectFirstRow() {
     const rows = document.getElementById("expressions-list").querySelectorAll("tr");
-    rows[1].className = 'bg-info';
+    rows[1].className = 'bg-info text-center';
+}
+
+function redirectToUpdate() {
+    window.location.href = "/updateExpression?id=" + selectedId;
+}
+
+function redirectToDelete() {
+    window.location.href = "/deleteExpression?id=" + selectedId;
 }
 
 window.onload = function() {
