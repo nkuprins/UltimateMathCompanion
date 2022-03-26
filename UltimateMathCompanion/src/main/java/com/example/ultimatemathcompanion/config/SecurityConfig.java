483d34c0c0c5e436233862a33668099e22def643
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-03-26 23:23:26 +0100

    Change dependency injection to constructor in SecurityConfig

--- UltimateMathCompanion/src/main/java/com/example/ultimatemathcompanion/config/SecurityConfig.java
+++ UltimateMathCompanion/src/main/java/com/example/ultimatemathcompanion/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.ultimatemathcompanion.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +14,11 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource securityDataSource;
+    private final DataSource securityDataSource;
+
+    public SecurityConfig(DataSource securityDataSource) {
+        this.securityDataSource = securityDataSource;
+    }
 
     @Override
     public void configure(WebSecurity web) {
